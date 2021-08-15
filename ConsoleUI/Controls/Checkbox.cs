a10bc8e8bd4f358a524e45b5dc2eb13484e9e113
HOTAPIYC <andreas_postkasten@gmx.net>
2021-08-15 12:27:32 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleUI/Controls/Checkbox.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ConsoleUI.Controls
+{
+    public class Checkbox
+    {
+        private CheckboxOption[] _options;
+
+        public Checkbox(CheckboxOption[] options)
+        {
+            _options = options;
+        }
+
+        public CheckboxOption[] Show()
+        {
+            var selectedIndex = 0;
+            var confirm = false;
+
+            while (true)
+            {
+                for (var i = 0; i < _options.Length; i++)
+                {
+                    if (i == selectedIndex && !confirm)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+
+                    Console.WriteLine($"{GetSelectorField(_options[i])} {_options[i].Text}");
+                    Console.ResetColor();
+                }
+
+                if (confirm)
+                {
+                    break;
+                }
+
+                var key = Console.ReadKey().Key;
+
+                if (key == ConsoleKey.DownArrow && selectedIndex < _options.Length)
+                {
+                    selectedIndex++;
+                }
+                if (key == ConsoleKey.UpArrow && selectedIndex > 0)
+                {
+                    selectedIndex--;
+                }
+                if (key == ConsoleKey.Spacebar)
+                {
+                    _options[selectedIndex].Selected = !_options[selectedIndex].Selected;
+                }
+                if (key == ConsoleKey.Enter)
+                {
+                    confirm = true;
+                }
+
+                Console.SetCursorPosition(0, Console.CursorTop - _options.Length);
+            }
+
+            string GetSelectorField(CheckboxOption option)
+            {
+                if (option.Selected)
+                {
+                    return "[*]";
+                }
+                else
+                {
+                    return "[ ]";
+                }
+            }
+
+            return _options;
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleUI.Controls;
+using System;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CheckboxOption[] options = {
+                new CheckboxOption("Option 1", true),
+                new CheckboxOption("Option 2"),
+                new CheckboxOption("Option 3")
+            };
+
+            var checkbox = new Checkbox(options);
+            checkbox.Show();
+        }
+    }
+}
